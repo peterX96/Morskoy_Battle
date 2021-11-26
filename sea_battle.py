4591ea72a4cfd9eff82d54305e2244d07fd530c8
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2021-11-26 10:30:15 +0300

    Update sea_battle.py

--- sea_battle.py
+++ sea_battle.py
@@ -119,8 +119,7 @@ class additional_background():
 
     def _init_battlefield(self):
 
-        self.battlefield = [[0] * 12 for i in range(12)]
-        self.battlefield2 = [[0] * 12 for i in range(12)]
+        self.battlefield = [[[0] * 12 for i in range(12)],[[0] * 12 for i in range(12)]]
 
     def _init_ship(self, motion_button):
         self.ship = Ship_Buttons()
@@ -149,27 +148,28 @@ class additional_background():
 
         return motion_button
       
-    def auto_set_ship(self):
+    def auto_set_ship(self,n):
         size_ship = [3,2,1,0]
+        battlefield_ = [[0] * 12 for i in range(12)]
         for n in ship_catalog:
             for m in range(n):
                 flag_angle = choice([0,1])
                 A = []
                 m,k = 11,11 - size_ship[n-1]
+                
                 if (flag_angle == 1): m,k = k,m
 
                 for i in range(1,m):
                     for j in range(1,k):
-                        if ((flag_angle ==0 and battlefield[i][j] == 0 and battlefield[i][j + size_ship[n-1]] == 0) or (flag_angle == 1 and battlefield[i][j] == 0 and battlefield[i + size_ship[n-1]][j] == 0)):
+                        if ((flag_angle ==0 and battlefield_[i][j] == 0 and battlefield_[i][j + size_ship[n-1]] == 0) or (flag_angle == 1 and battlefield_[i][j] == 0 and battlefield_[i + size_ship[n-1]][j] == 0)):
                                 A.append((i,j))
 
                 coord = choice(A)
-
                 for i in range(0,size_ship[n-1] + 1):
                     if (flag_angle == 0):
-                        self.battlefield[coord[0]][coord[1] + i] = size_ship[n-1] + 2
+                        battlefield_[coord[0]][coord[1] + i] = size_ship[n-1] + 2
                     else:
-                        self.battlefield[coord[0]+i][coord[1]] = size_ship[n-1] + 2
+                        battlefield_[coord[0]+i][coord[1]] = size_ship[n-1] + 2
 
                 if (n == 1) : B = Battleship
                 elif (n == 2) : B = Carrier
@@ -178,11 +178,13 @@ class additional_background():
 
                 for i in range(len(B)):
                     if (flag_angle == 0):
-                        battlefield[int(coord[0] + B[i][1])][int(coord[1] + B[i][0])] = 1
+                        battlefield_[int(coord[0] + B[i][1])][int(coord[1] + B[i][0])] = 1
                     else:
-                        battlefield[int(coord[0] + B[i][0])][int(coord[1] + B[i][1])] = 1
+                        battlefield_[int(coord[0] + B[i][0])][int(coord[1] + B[i][1])] = 1
                         
+        self.battlefield[n] = battlefield_
         return [0,0,0,0]
+      
 class Servise_Function:
     def battleground(self,x,y,n):
 
