79939e8272f72ffa91acb4e53e8455fdafda06b3
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2021-11-26 10:17:22 +0300

    Update sea_battle.py

--- sea_battle.py
+++ sea_battle.py
@@ -28,7 +28,12 @@ post_pressing_effect = [[(1, 0, 0), (0, 1, 0), (0, 8, 0)], [
     (0, 2, 0), (0, 5, 0)], [(0, 2, 1), (0, 2, 2), (0, 2, 3), (0, 2, 4), (0, 3, 0)], [(0, 2, 0), (0, 0, 0), (1, 0, 0)]]
 
 Map = [(100,100),(500,500)]
-S_0 = [[(-1,-1),(0,-1),(1,-1),(2,-1),(3,-1),(4,-1),(4,0),(4,1),(3,1),(2,1),(1,1),(0,1),(-1,1),(-1,0)],[(-1,-1),(0,-1),(1,-1),(2,-1),(3,-1),(3,0),(3,1),(2,1),(1,1),(0,1),(-1,1),(-1,0)],[(-1,-1),(0,-1),(1,-1),(2,-1),(2,0),(2,1),(1,1),(0,1),(-1,1),(-1,0)],[(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1),(-1,0)]]
+
+Battleship = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(2,1),(3,1),(4,1),(4,0),(4,-1),(3,-1),(2,-1),(1,-1),(0,-1)]
+Carrier = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(2,1),(3,1),(3,0),(3,-1),(2,-1),(1,-1),(0,-1)]
+Cruiser = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(2,1),(2,0),(2,-1),(1,-1),(0,-1)]
+Destroyer = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1)]
+
 class Button:
     def _init(self, n):
 
@@ -147,23 +152,35 @@ class additional_background():
     def auto_set_ship(self):
         size_ship = [3,2,1,0]
         for n in ship_catalog:
-            for m in range(i):
-                self.flag_angle = choice(0,1)
+            for m in range(n):
+                flag_angle = choice([0,1])
                 A = []
-                if (self.flag_angle == 0):
-                    m,n = 11,11 - size_ship[n-1]
-                else:
-                    n,m = 11,11 - size_ship[n-1]
+                m,k = 11,11 - size_ship[n-1]
+                if (flag_angle == 1): m,k = k,m
 
                 for i in range(1,m):
-                    for j in range(1,n):
-                        if (self.battlefield[i][j] == 0):
-                            A.append((i,j))
+                    for j in range(1,k):
+                        if ((flag_angle ==0 and battlefield[i][j] == 0 and battlefield[i][j + size_ship[n-1]] == 0) or (flag_angle == 1 and battlefield[i][j] == 0 and battlefield[i + size_ship[n-1]][j] == 0)):
+                                A.append((i,j))
 
-                self.coord = choice(A)
+                coord = choice(A)
 
-                for i in range(size_ship[n-1] + 1):
-                    self.battlefi
+                for i in range(0,size_ship[n-1] + 1):
+                    if (flag_angle == 0):
+                        self.battlefield[coord[0]][coord[1] + i] = size_ship[n-1] + 2
+                    else:
+                        self.battlefield[coord[0]+i][coord[1]] = size_ship[n-1] + 2
+
+                if (n == 1) : B = Battleship
+                elif (n == 2) : B = Carrier
+                elif (n == 3) : B = Cruiser
+                else: B = Destroyer
+
+                for i in range(len(B)):
+                    if (flag_angle == 0):
+                        battlefield[int(coord[0] + B[i][1])][int(coord[1] + B[i][0])] = 1
+                    else:
+                        battlefield[int(coord[0] + B[i][0])][int(coord[1] + B[i][1])] = 1
         
 
 class Servise_Function:
