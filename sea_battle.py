dae6fe11c2f125c8f1a824c1a88884c41e5ae701
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2021-11-29 23:34:44 +0300

    Update sea_battle.py

--- sea_battle.py
+++ sea_battle.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.constants import MOUSEBUTTONDOWN
 from pygame.draw import *
 from random import choice
 import json
@@ -22,10 +23,14 @@ button_coord = [[[(902, 105), (100, -100)], [(432, 522), (137, -143)], [(902, 20
                  [(900, 105), (100, -100)], [(845, 740), (150, -50)], [(697, 740), (150, -50)], [(197, 803), (300, -50)]],
                 [[(229, 477), (543, -63)], [(229, 544), (544, -62)], [(229, 610), (544, -62)]]]
 
+attack_image = ['images_static_battleground/hit_on_sea.png','images_static_battleground/hit_on_ship.png']
+
 background_image = ['images_static_battleground/menu_game.png',
                     'images_static_battleground/question_about_gamemode.png',
                     'images_static_battleground/selection_of_ships.png',
-                    'images_static_battleground/question_quit.png']
+                    'images_static_battleground/question_quit.png',
+                    'images_static_battleground/field_of_attack_player.png',
+                    'images_static_battleground/field_of_defend_player.png']
 post_pressing_effect = [[(1, 0, 0), (0, 1, 0), (0, 8, 0)], [
     (0, 2, 0), (0, 5, 0)], [(0, 2, 1), (0, 2, 2), (0, 2, 3), (0, 2, 4), (0, 3, 0), (0, 2, -1), (0, 2, -2), (0, 2, -3)], [(0, 2, 0), (0, 0, 0), (1, 0, 0)]]
 
@@ -127,28 +132,24 @@ def operator_on_buttons(number, screen_id, event):
                 a, b, c = post_pressing_effect[screen_id][i]
     return a, b, c
 
-class Additional_background():
+class Battlefield():
 
     def _init_battlefield(self):
 
         self.battlefield = [
             [[0] * 12 for i in range(12)], [[0] * 12 for i in range(12)]]
-        self.coordinates = []
-        self.flag_angle = []
-        self.id = []
-        self.angle = []
-        self.flag = 0
-        self.catalog = [1, 2, 3, 4]
+        self.coordinates , self.flag_angle = [[] * 2 for i in range(2)], [[] * 2 for i in range(2)]
+        self.id, self.angle = [[] * 2 for i in range(2)], [[] * 2 for i in range(2)]
+        self.flag = [0,0]
+        self.catalog = [[1, 2, 3, 4], [1 , 2, 3, 4]]
 
     def clear_battlefield(self, n):
 
         self.battlefield[n] = [[0] * 12 for i in range(12)]
-        self.coordinates = []
-        self.flag_angle = []
-        self.id = []
-        self.angle = []
-        self.flag = 0
-        self.catalog = [1, 2, 3, 4]
+        self.coordinates[n] , self.flag_angle[n] = [], []
+        self.id[n], self.angle[n] = [], []
+        self.flag[n] = 0
+        self.catalog[n] = [1, 2, 3, 4]
 
     def _init_ship(self, _ship):
         self.ship = Ship_Buttons()
@@ -156,7 +157,7 @@ class Additional_background():
 
     def operator_on_ships_button(self, _ship, event, N):
         _ship_ = _ship + 1
-        if (self.catalog[_ship] > 0):
+        if (self.catalog[N][_ship] > 0):
             if event.type == pygame.MOUSEMOTION:
                 self.ship._draw(event)
 
@@ -169,14 +170,14 @@ class Additional_background():
                     _ship_ = 0
 
                 elif (event.button == 1):
-                    self.catalog[_ship] -= 1
+                    self.catalog[N][_ship] -= 1
                     x, y, angle_flag, d, angle = self.ship.examination_of_button(
                         event, _ship)
                     if (angle_flag != -1):
 
                         flag = True
                         for i in range(size_ship[_ship] + 1):
-                            if ((angle_flag == 0 and self.battlefield[N][y][x+i] != 0) or (angle_flag == 1 and self.battlefield[N][y][x+i] != 0)):
+                            if ((angle_flag == 0 and self.battlefield[N][y][x+i] != 0) or (angle_flag == 1 and self.battlefield[N][y+i][x] != 0)):
                                 flag = False
                                 break
 
@@ -204,15 +205,15 @@ class Additional_background():
                                     self.battlefield[N][y + Ships[_ship]
                                                         [i][0]][x + Ships[_ship][i][1]] = 1
 
-                            self.coordinates.append([y, x])
-                            self.flag_angle.append(angle_flag)
-                            self.id.append(d)
-                            self.angle.append(angle)
-                            self.flag = 1
+                            self.coordinates[N].append([y, x])
+                            self.flag_angle[N].append(angle_flag)
+                            self.id[N].append(d)
+                            self.angle[N].append(angle)
+                            self.flag[N] = 1
                         else:
-                            self.catalog[_ship] += 1
+                            self.catalog[N][_ship] += 1
                     else:
-                        self.catalog[_ship] += 1
+                        self.catalog[N][_ship] += 1
                 self.ship._draw(event)
         else:
             _ship_ = 0
@@ -220,7 +221,7 @@ class Additional_background():
 
     def auto_set_ship(self, N):
 
-        for n in self.catalog:
+        for n in self.catalog[N]:
             for m in range(n):
                 flag_angle = choice([0, 1])
                 A = []
@@ -236,10 +237,10 @@ class Additional_background():
 
                 coord = choice(A)
 
-                self.coordinates.append(coord)
-                self.flag_angle.append(flag_angle)
-                self.id.append(n-1)
-                self.angle.append(180*choice([0, 1]))
+                self.coordinates[N].append(coord)
+                self.flag_angle[N].append(flag_angle)
+                self.id[N].append(n-1)
+                self.angle[N].append(180*choice([0, 1]))
 
                 for i in range(0, size_ship[n-1] + 1):
                     if (flag_angle == 0):
@@ -259,40 +260,54 @@ class Additional_background():
                         self.battlefield[N][int(
                             coord[0] + B[i][0])][int(coord[1] + B[i][1])] = 1
 
-        self.flag = 1
-        self.catalog = [0, 0, 0, 0]
+        self.flag[N] = 1
+        self.catalog[N] = [0, 0, 0, 0]
 
-    def draw_battleground(self):
+    def draw_battleground(self,N):
 
         if (screen_id == 2):
             for i in range(0, 400, 100):
-                s_f.text(950, 170 + i, str(self.catalog[i//100]), BLACK, 64)
+                s_f.text(950, 170 + i, str(self.catalog[N][i//100]), BLACK, 64)
 
         s_f.battleground(x0, y0, 10 * delta)
 
-        if (self.flag == 1):
-            for i in range(len(self.id)):
-                _x = pygame.image.load(ships_images[self.id[i]])
-                x, y = self.coordinates[i][1] - 1, self.coordinates[i][0] - 1
-                half_size_ship = (size_ship[self.id[i]] + 1) * delta / 2
-                if (self.flag_angle[i] == 0):
+        if (self.flag[N] == 1):
+            for i in range(len(self.id[N])):
+                _x = pygame.image.load(ships_images[self.id[N][i]])
+                x, y = self.coordinates[N][i][1] - 1, self.coordinates[N][i][0] - 1
+                half_size_ship = (size_ship[self.id[N][i]] + 1) * delta / 2
+                if (self.flag_angle[N][i] == 0):
                     center_ship = (x * delta + x0 + half_size_ship,
                                    y * delta + delta / 2 + y0)
                     _x, x_rect = s_f.rot_center(
-                        _x, self.angle[i], center_ship[0], center_ship[1])
+                        _x, self.angle[N][i], center_ship[0], center_ship[1])
 
                 else:
                     center_ship = (x * delta + x0 + delta / 2,
                                    y * delta + half_size_ship + y0)
                     _x, x_rect = s_f.rot_center(
-                        _x, 90 + self.angle[i], center_ship[0], center_ship[1])
+                        _x, 90 + self.angle[N][i], center_ship[0], center_ship[1])
 
                 x_rect = _x.get_rect(center=center_ship)
                 screen.blit(_x, x_rect)
 
-    def continue_button(self):
-        return (int(self.catalog == [0, 0, 0, 0]))
+    def hiding_ships(self,N):
+        self.flag[N] = 0
 
+    def dehiding_ships(self,N):
+        self.flag[N] = 1
+
+    def attack_on_ships(self,event,N):
+        if (event.type == pygame.MOUSEBUTTONDOWN ):
+            if ( x0 <= event.pos[0] and event.pos[0] <= x0 + 10 * delta and y0 <= event.pos[1] and event.pos[1] <= y0 + 10 * delta ):
+                a,b = (event.pos[0] - x0) // delta + 1, (event.pos[1]-y0) // delta + 1
+
+                if (self.battlefield[N][b][a] > 1 ):
+                    self.battlefield[N][b][a] = 999
+                
+    def continue_button(self,N):
+        return (int(self.catalog[N] == [0, 0, 0, 0]))
+    
     def print_battlefield(self): #these function for only view
         print('First Field')
         for row in self.battlefield[0]:
@@ -300,6 +315,9 @@ class Additional_background():
         print('Second Field')
         for row in self.battlefield[1]:
             print(' '.join([str(elem) for elem in row]))
+
+        print(self.coordinates[0])
+        print(self.coordinates[1])
 
 def operator_on_screen(screen_id):
     old_screen_id = screen_id
@@ -343,17 +361,14 @@ clock = pygame.time.Clock()
 finished = False
 
 
-screen_id = 0
-flag_quit = 0
-gamemode = 0
-ship_choice = 0
+screen_id , flag_quit , gamemode , ship_choice = 0 , 0 , 0 , 0
 _ship_catalog = [1, 2, 3, 4]
 ship_catalog = _ship_catalog
 size_ship = [3, 2, 1, 0]
 
 s_f = Servise_Function()
 b_n = Button()
-add = Additional_background()
+add = Battlefield()
 
 x0, y0 = 100, 100
 delta = 50
@@ -366,7 +381,7 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or flag_quit == 1:
             finished = True
-        elif screen_id != 2 and screen_id != 4:
+        elif screen_id != 2 and screen_id != 4 and screen_id != 5:
 
             if (screen_id != 3):
                 add._init_battlefield()
@@ -376,7 +391,7 @@ while not finished:
         elif screen_id == 2:  # SET YOUR SHIPS
 
             static_background(screen_id)
-            add.draw_battleground()
+            add.draw_battleground(0)
 
             if (ship_choice == 0):
                 flag_quit, screen_id, ship_choice = operator_on_screen(
@@ -409,15 +424,28 @@ while not finished:
                     flag_fill = True
 
                 elif (ship_choice == -3):
-                    if (add.continue_button()):
-                        screen_id = 4
+                    if (add.continue_button(0)):
+                        screen_id = 4 
                         add.clear_battlefield(1)
                         add.auto_set_ship(1)
+
+                        flag_move = True
+
+                        static_background(screen_id)
                         add.print_battlefield()
+                        x0,y0 = 250,150
                     ship_choice = 0
 
         elif (screen_id == 4): # Here should be myasso
-            rect(screen, (255, 255, 255), (0, 0, 1000, 1000), 0)
+            
+            add.hiding_ships(1)
+            add.draw_battleground(1)
+
+            add.attack_on_ships(event,1)
+
+            pass
+        elif (screen_id == 5):
+            add.draw_battleground(0)
 
     pygame.display.update()
     pygame.display.set_caption("Sea Battle")
